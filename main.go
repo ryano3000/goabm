25e164c95f1135e217781e20a1e36a69430d09e7
VasanthaKumar <vasanthakumar@LAPSZ3359.ird.mu-sigma.com>
2019-01-31 12:02:32 +0530

    grpc server in main

--- main.go
+++ main.go
@@ -1,28 +1,53 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+	"time"
 
-	"github.com/Vasanthakumar.V/goab/internal/shopper"
-	"github.com/Vasanthakumar.V/goab/pkgs/abm"
+	"github.com/VasanthakumarV/goabm/apis/grpc/proto"
+	"github.com/VasanthakumarV/goabm/internal/shopper"
+	"github.com/VasanthakumarV/goabm/pkgs/abm"
+	"google.golang.org/grpc"
 )
 
-func main() {
+type server struct{}
+
+func (*server) Status(req *agent.AgentStatusRequest, stream agent.AgentStatusService_StatusServer) error {
+
 	// Create a world with arg which specify
-	// the buffer size for the channel and the max number of 
+	// the buffer size for the channel and the max number of
 	// goroutines
 	w := abm.NewWorld(1, 2)
 
 	// Create slice of agents to send in
 	ss := []abm.Agent{&shopper.Agent{}, &shopper.Agent{}}
 
-	// Run the simulation for 5 ticks and 
+	// Run the simulation for 5 ticks and
 	in := w.Start(5, ss)
 
 	// Read in and print the changed status of agents
 	// from the channel returned after start
 	for s := range in {
-		ss := s.(*shopper.Agent)
-		fmt.Printf("%#v\n", *ss)
+		status := s.(*shopper.Agent)
+		res := &agent.AgentStatusResponse{
+			Status: status.Status,
+		}
+		stream.Send(res)
+		time.Sleep(time.Second)
+	}
+
+	return nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatal("Failed to listen")
+	}
+	s := grpc.NewServer()
+	agent.RegisterAgentStatusServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("Failed to serve")
 	}
 }
